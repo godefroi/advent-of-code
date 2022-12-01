ecb78864d6d3f750b3f4293d914b1c67db05ca5e
Mark Parker <mparker@medicity.com>
2022-12-01 13:49:32 -0700

    Add solution for day 01

---
+++ Day01/Problem.cs
@@ -0,0 +1,79 @@
+﻿namespace Day01;
+
+public class Problem
+{
+	internal static (long max, long topThree) Main(string fileName)
+	{
+		var lines = File.ReadAllLines(GetFilePath(fileName));
+		var elves = ChunkByEmpty(lines).Select(group => group.Select(s => long.Parse(s)).Sum()).OrderDescending().ToList();
+		var ret   = (elves.First(), elves.Take(3).Sum());
+
+		Console.WriteLine(ret);
+		return ret;
+	}
+
+	private static IEnumerable<IEnumerable<string>> ChunkByEmpty(IEnumerable<string> lines)
+	{
+		var doneEnumerating = false;
+
+		using var enumerator = lines.GetEnumerator();
+
+		while (!doneEnumerating) {
+			yield return EnumerateWhileNotEmpty(enumerator);
+		}
+
+		IEnumerable<string> EnumerateWhileNotEmpty(IEnumerator<string> enumerator)
+		{
+			while (enumerator.MoveNext()) {
+				if (enumerator.Current == string.Empty) {
+					yield break;
+				} else {
+					yield return enumerator.Current;
+				}
+			}
+
+			doneEnumerating = true;
+		}
+	}
+
+	[Fact]
+	public void SampleInputReturnsCorrectValues()
+	{
+		var (max, topThree) = Main("inputSample.txt");
+
+		Assert.Equal(24000, max);
+		Assert.Equal(45000, topThree);
+	}
+
+	[Fact]
+	public void InputReturnsCorrectValues()
+	{
+		var (max, topThree) = Main("input.txt");
+
+		Assert.Equal(66616, max);
+		Assert.Equal(199172, topThree);
+	}
+
+	[Fact]
+	public void ChunkingWorksCorrectly()
+	{
+		var chunks = ChunkByEmpty(File.ReadAllLines(GetFilePath("inputSample.txt"))).Select(e => e.ToList()).ToList();
+
+		Assert.Collection(chunks,
+			chunk => Assert.Collection(chunk,
+				l => l.Equals("1000"),
+				l => l.Equals("2000"),
+				l => l.Equals("3000")),
+			chunk => Assert.Collection(chunk,
+				l => l.Equals("4000")),
+			chunk => Assert.Collection(chunk,
+				l => l.Equals("5000"),
+				l => l.Equals("6000")),
+			chunk => Assert.Collection(chunk,
+				l => l.Equals("7000"),
+				l => l.Equals("8000"),
+				l => l.Equals("9000")),
+			chunk => Assert.Collection(chunk,
+				l => l.Equals("10000")));
+	}
+}
---
+++ Tools.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace aoc_2022;
+
+public static class Tools
+{
+	public static string GetFilePath(string fileName, [CallerFilePath]string sourceFilePath = "") => Path.Combine(Path.GetDirectoryName(sourceFilePath)!, fileName);
+}
