42237239f7d2f848f17d016b0b3eb1aebbd45250
Mark Parker <mparker@medicity.com>
2022-12-04 06:06:30 -0700

    Add day 3 solution

--- Tools.cs
+++ Tools.cs
@@ -7,4 +7,6 @@ public static class Tools
 	public static string GetFilePath(string fileName, [CallerFilePath]string sourceFilePath = "") => Path.Combine(Path.GetDirectoryName(sourceFilePath)!, fileName);
 
 	public static string[] ReadFileLines(string fileName, [CallerFilePath]string sourceFilePath = "") => File.ReadAllLines(GetFilePath(fileName, sourceFilePath));
+
+	public static T[] ReadFileLines<T>(string fileName, Func<string, T> selector, [CallerFilePath]string sourceFilePath = "") => File.ReadAllLines(GetFilePath(fileName, sourceFilePath)).Select(selector).ToArray()
 }
