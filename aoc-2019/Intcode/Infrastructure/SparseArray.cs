e6b0b6333bf702f5c600cc813f5b72b80353d670
Mark Parker <mparker@medicity.com>
2022-12-06 12:21:24 -0700

    Add 2019 day 02 solution

--- aoc-2019/Intcode/Infrastructure/SparseArray.cs
+++ aoc-2019/Intcode/Infrastructure/SparseArray.cs
@@ -1,34 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
-namespace aoc_2019.Intcode.Infrastructure
+namespace aoc_2019.Intcode.Infrastructure;
+
+public class SparseArray<T>
 {
-	internal class SparseArray<T>
+	private readonly Dictionary<long, T> _values = new();
+
+	public SparseArray(IEnumerable<T> initialValues)
 	{
-		private Dictionary<long, T> m_values = new Dictionary<long, T>();
+		var idx = 0;
 
-		public SparseArray(IEnumerable<T> initialValues)
-		{
-			var idx = 0;
+		foreach (var v in initialValues) {
+			_values[idx++] = v;
+		}
+	}
 
-			foreach( var v in initialValues )
-				m_values[idx++] = v;
+	public long Length => _values.Keys.Max();
+
+	public T? this[long index]
+	{
+		get {
+			if (_values.TryGetValue(index, out var value)) {
+				return value;
+			} else return default;
+		}
+		set {
+			if (value == null) {
+				_values.Remove(index);
+			} else {
+				_values[index] = value;
+			}
+		}
+	}
+
+	public override string ToString()
+	{
+		var sb      = new StringBuilder("[");
+		var lastKey = -1L;
+
+		foreach (var key in _values.Keys.Order()) {
+			if (lastKey > -1) {
+				sb.Append(',');
+			}
+
+			sb.Append(this[key]);
+
+			lastKey = key;
 		}
 
-		public int Length => m_values.Count;
+		sb.Append(']');
 
-		public T this[long index]
-		{
-			get {
-				if( m_values.ContainsKey(index) )
-					return m_values[index];
-				else
-					return default(T);
-			}
-			set {
-				m_values[index] = value;
-			}
-		}
+		return sb.ToString();
 	}
 }
