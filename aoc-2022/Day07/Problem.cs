d51040103f89ac0516593030c0660c5a55664296
Mark Parker <mparker@medicity.com>
2022-12-07 08:12:44 -0700

    Add 2022 day 07 solution

---
+++ aoc-2022/Day07/Problem.cs
@@ -0,0 +1,76 @@
+﻿namespace aoc_2022.Day07;
+
+public class Problem
+{
+	private const int TOTAL_SPACE  = 70000000;
+	private const int NEEDED_SPACE = 30000000;
+	
+	public static (int, int) Main(string fileName)
+	{
+		//fileName = "inputSample.txt";
+
+		var root        = new Directory("/");
+		var directories = new List<Directory>() { root };
+		var current     = root;
+
+		foreach (var line in ReadFileLines(fileName)) {
+			if (line == "$ cd /") {
+				// already made the root directory
+				current = root;
+			} else if (line.StartsWith("$ cd ")) {
+				// directory change
+				var name = line[5..];
+				current = name == ".." ? current!.Parent : current!.Children.Single(d => d.Name == name);
+			} else if (line.StartsWith("dir ")) {
+				// add a directory to this directory
+				var dir = new Directory(line[4..]);
+				directories.Add(dir);
+				current!.AddChild(dir);
+			} else if (line.StartsWith("$ ls")) {
+				// do nothing, we ignore this
+			} else {
+				// add a file to this directory
+				var parts = line.Split(' ', 2);
+				current!.Files.Add(KeyValuePair.Create(parts[1], int.Parse(parts[0])));
+			}
+		}
+
+		var part1      = directories.Where(d => d.Size <= 100000).Sum(d => d.Size);
+		var needToFree = NEEDED_SPACE - (TOTAL_SPACE - root.Size);
+		var part2      = directories.OrderBy(d => d.Size).First(d => d.Size >= needToFree).Size;
+
+		return (part1, part2);
+	}
+
+	public class Directory
+	{
+		//private readonly Lazy<int> _size;
+
+		public Directory(string name)
+		{
+			Children = new List<Directory>();
+			Files    = new List<KeyValuePair<string, int>>();
+			Name     = name;
+
+			//_size = new(() => Files.Sum(f => f.Value) + Children.Sum(c => c.Size));
+		}
+
+		public string Name { get; }
+
+		public Directory? Parent { get; private set; }
+
+		public IList<Directory> Children { get; }
+
+		public IList<KeyValuePair<string, int>> Files { get; }
+
+		//public int Size => _size.Value;
+		public int Size => Files.Sum(f => f.Value) + Children.Sum(c => c.Size);
+
+		public void AddChild(Directory child)
+		{
+			Children.Add(child);
+
+			child.Parent = this;
+		}
+	}
+}
