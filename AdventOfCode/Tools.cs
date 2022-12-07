54eec3d56c3e1b0a7bc5da6e10f03b46f24c31e2
Mark Parker <mparker@medicity.com>
2022-12-07 12:43:31 -0700

    add 2019 day 12 solution

--- AdventOfCode/Tools.cs
+++ AdventOfCode/Tools.cs
@@ -9,4 +9,11 @@ public static class Tools
 	public static string[] ReadFileLines(string fileName, [CallerFilePath]string sourceFilePath = "") => File.ReadAllLines(GetFilePath(fileName, sourceFilePath));
 
 	public static T[] ReadFileLines<T>(string fileName, Func<string, T> selector, [CallerFilePath]string sourceFilePath = "") => File.ReadAllLines(GetFilePath(fileName, sourceFilePath)).Select(selector).ToArray();
+
+	public static long LeastCommonMultiple(long[] numbers) => numbers.Aggregate(LeastCommonMultiple);
+
+	public static long LeastCommonMultiple(long a, long b) => Math.Abs(a * b) / GreatestCommonDenominator(a, b);
+
+	public static long GreatestCommonDenominator(long a, long b) => b == 0 ? a : GreatestCommonDenominator(b, a % b);
+
 }
