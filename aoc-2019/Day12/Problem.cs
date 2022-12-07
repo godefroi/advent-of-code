e47d58aa1fdb08c29796ee6f054995f1916a57fe
Mark Parker <mparker@medicity.com>
2022-12-07 12:43:31 -0700

    add 2019 day 12 solution

--- aoc-2019/Day12/Problem.cs
+++ aoc-2019/Day12/Problem.cs
@@ -1,149 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
+﻿using System.Text.RegularExpressions;
 
-namespace aoc_2019
+namespace aoc_2019.Day12;
+
+public partial class Problem
 {
-	internal static class problem_12
+	public static (long, long) Main(string fileName)
 	{
-		private static Moon[] m_input = new[] {
-				new Moon(0, new Vector3(-7, -8, 9)),
-				new Moon(1, new Vector3(-12, -3, -4)),
-				new Moon(2, new Vector3(6, -17, -9)),
-				new Moon(3, new Vector3(4, -10, -6)),
-			};
+		var moons = ReadFileLines(fileName, Parse).Select((vector, idx) => new Moon(idx, vector)).ToArray();
+		var part1 = Part1(moons);
+		var part2 = Part2(moons);
 
-		public static void Part1()
-		{
-			var moons = m_input;
+		return (part1, part2);
+	}
 
-			for( var n = 0; n < 1000; n++ ) {
-				// apply gravity
-				for( var i = 0; i < moons.Length; i++ ) {
-					foreach( var o in moons ) {
-						if( moons[i].Id != o.Id )
-							moons[i] = moons[i].ApplyGravity(o);
+	private static long Part1(Moon[] moons)
+	{
+		for (var n = 0; n < 1000; n++) {
+			// apply gravity
+			for (var i = 0; i < moons.Length; i++) {
+				foreach (var o in moons) {
+					if (moons[i].Id != o.Id) {
+						moons[i] = moons[i].ApplyGravity(o);
 					}
 				}
-
-				// apply velocity
-				moons = moons.Select(m => m.ApplyVelocity()).ToArray();
 			}
 
-			Console.WriteLine(moons.Sum(m => m.Energy));
+			// apply velocity
+			moons = moons.Select(m => m.ApplyVelocity()).ToArray();
 		}
 
-		public static void Part2()
-		{
-			var moons  = m_input;
+		return moons.Sum(m => m.Energy);
+	}
 
-			var mul_x = CalculateDimension(moons.Select(m => (int)m.Position.X));
-			var mul_y = CalculateDimension(moons.Select(m => (int)m.Position.Y));
-			var mul_z = CalculateDimension(moons.Select(m => (int)m.Position.Z));
+	private static long Part2(Moon[] moons)
+	{
+		var mul_x = CalculateDimension(moons.Select(m => (int)m.Position.X));
+		var mul_y = CalculateDimension(moons.Select(m => (int)m.Position.Y));
+		var mul_z = CalculateDimension(moons.Select(m => (int)m.Position.Z));
 
-			Console.WriteLine(LeastCommonMultiple(new long[] { mul_x, mul_y, mul_z }));
+		return LeastCommonMultiple(new long[] { mul_x, mul_y, mul_z });
+	}
+
+	private static Vector3 Parse(string input)
+	{
+		var match = VectorRegex().Match(input);
+
+		if (!match.Success) {
+			throw new InvalidDataException($"The input {input} was not parseable.");
 		}
 
-		private static int CalculateDimension(IEnumerable<int> positions)
-		{
-			var origin = positions.ToArray();
-			var pos    = origin.Clone() as int[];
-			var vel    = new int[pos.Length];
-			var cnt    = 0;
+		return new(long.Parse(match.Groups["x"].Value), long.Parse(match.Groups["y"].Value), long.Parse(match.Groups["z"].Value));
+	}
 
-			while( true ) {
-				for( var i = 0; i < pos.Length; i++ ) {
-					for( var j = 0; j < pos.Length; j++ ) {
-						if( pos[j] > pos[i] )
-							vel[i] += 1;
-						else if( pos[j] < pos[i] )
-							vel[i] -= 1;
+	private static int CalculateDimension(IEnumerable<int> positions)
+	{
+		var origin = positions.ToArray();
+		var pos    = (int[])origin.Clone();
+		var vel    = new int[pos.Length];
+		var cnt    = 0;
+
+		while (true) {
+			for (var i = 0; i < pos.Length; i++) {
+				for (var j = 0; j < pos.Length; j++) {
+					if (pos[j] > pos[i]) {
+						vel[i] += 1;
+					} else if (pos[j] < pos[i]) {
+						vel[i] -= 1;
 					}
 				}
-
-				for( var i = 0; i < pos.Length; i++ )
-					pos[i] += vel[i];
-
-				cnt++;
-
-				if( vel.All(v => v == 0) && pos.SequenceEqual(origin) )
-					return cnt;
-			}
-		}
-
-		private static long LeastCommonMultiple(long[] numbers) => numbers.Aggregate(LeastCommonMultiple);
-
-		private static long LeastCommonMultiple(long a, long b) => Math.Abs(a * b) / GreatestCommonDenominator(a, b);
-
-		private static long GreatestCommonDenominator(long a, long b) => b == 0 ? a : GreatestCommonDenominator(b, a % b);
-
-		internal class Moon
-		{ 
-			public Moon(int id, Vector3 position) : this(id, position, Vector3.Zero) { }
-
-			public Moon(int id, Vector3 position, Vector3 velocity)
-			{
-				Id       = id;
-				Position = position;
-				Velocity = velocity;
 			}
 
-			public int Id { get; }
-
-			public Vector3 Position { get; }
-
-			public Vector3 Velocity { get; }
-
-			public long Energy => Position.Magnitude * Velocity.Magnitude;
-
-			public Moon ApplyGravity(Moon other)
-			{
-				var chg_x = other.Position.X > Position.X ? 1 : other.Position.X < Position.X ? -1 : 0;
-				var chg_y = other.Position.Y > Position.Y ? 1 : other.Position.Y < Position.Y ? -1 : 0;
-				var chg_z = other.Position.Z > Position.Z ? 1 : other.Position.Z < Position.Z ? -1 : 0;
-
-				return new Moon(Id, Position, Velocity + new Vector3(chg_x, chg_y, chg_z));
+			for (var i = 0; i < pos.Length; i++) {
+				pos[i] += vel[i];
 			}
 
-			public Moon ApplyVelocity() => new Moon(Id, Position + Velocity, Velocity);
+			cnt++;
 
-			public override string ToString() => $"pos=<x={Position.X,3}, y={Position.Y,3}, z={Position.Z,3}>, vel=<x={Velocity.X,3}, y={Velocity.Y,3}, z={Velocity.Z,3}>";
-		}
-
-		internal class Vector3
-		{
-			public Vector3(long x, long y, long z)
-			{
-				X = x;
-				Y = y;
-				Z = z;
+			if (vel.All(v => v == 0) && pos.SequenceEqual(origin)) {
+				return cnt;
 			}
-
-			public long X { get; }
-
-			public long Y { get; }
-
-			public long Z { get; }
-
-			public long Magnitude => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
-
-			public static Vector3 operator +(Vector3 v1, Vector3 v2) => new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
-
-			public static Vector3 Zero { get; } = new Vector3(0, 0, 0);
-		}
-
-		internal class Dimension
-		{
-			public Dimension(int position, int velocity)
-			{
-				Position = position;
-				Velocity = velocity;
-			}
-
-			public int Position;
-			public int Velocity;
 		}
 	}
+
+	internal readonly record struct Moon(int Id, Vector3 Position, Vector3 Velocity)
+	{ 
+		public Moon(int id, Vector3 position) : this(id, position, Vector3.Zero) { }
+
+		public long Energy => Position.Magnitude * Velocity.Magnitude;
+
+		public Moon ApplyGravity(Moon other) => new(Id, Position, Velocity + new Vector3(
+			(other.Position.X - Position.X) switch { > 0 => 1, < 0 => -1, 0 => 0 },
+			(other.Position.Y - Position.Y) switch { > 0 => 1, < 0 => -1, 0 => 0 },
+			(other.Position.Z - Position.Z) switch { > 0 => 1, < 0 => -1, 0 => 0 }));
+
+		public Moon ApplyVelocity() => new(Id, Position + Velocity, Velocity);
+	}
+
+	internal readonly record struct Vector3(long X, long Y, long Z)
+	{
+		public long Magnitude => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+
+		public static Vector3 operator +(Vector3 v1, Vector3 v2) => new(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+
+		public static Vector3 Zero { get; } = new Vector3(0, 0, 0);
+	}
+
+	[GeneratedRegex(@"\<x=(?<x>[-\d]+), y=(?<y>[-\d]+), z=(?<z>[-\d]+)\>")]
+	private static partial Regex VectorRegex();
 }
