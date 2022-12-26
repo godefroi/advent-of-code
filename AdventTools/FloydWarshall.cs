ad4371622be9b2cdec0153ea4129a9bf23186158
Mark Parker <mparker@medicity.com>
2022-12-26 09:29:23 -0700

    Add 2022 day 16 solution

---
+++ AdventTools/FloydWarshall.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode;
+
+public class FloydWarshall
+{
+	/// <summary>
+	/// Compute the shortest distance between all nodes of a directed, weighted graph with positive or negative edge weights (but no negative cycles)
+	/// </summary>
+	/// <typeparam name="T">The type of <see cref="Edge{T}"/> being used</typeparam>
+	/// <typeparam name="W">Tye type of weight being used</typeparam>
+	/// <param name="edges">The set of weighted edges in the graph</param>
+	/// <remarks>
+	/// https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm#Algorithm
+	/// https://olegkarasik.wordpress.com/2021/04/25/implementing-floyd-warshall-algorithm-for-solving-all-pairs-shortest-paths-problem-in-c/
+	/// </remarks>
+	public static Dictionary<Edge<T>, W> ComputeDistances<T, W>(IEnumerable<WeightedEdge<T, W>> edges) where W : INumber<W>
+	{
+		var nodeSet = new HashSet<T>();
+		var dist = new Dictionary<Edge<T>, W>();
+
+		// set the distance for the edges we know
+		foreach (var edge in edges) {
+			nodeSet.Add(edge.Edge.From);
+			nodeSet.Add(edge.Edge.To);
+			dist.Add(edge.Edge, edge.Weight);
+		}
+
+		// nodes are all zero distance from themselves
+		foreach (var node in nodeSet) {
+			dist.Add(new Edge<T>(node, node), W.Zero);
+		}
+
+		foreach (var k in nodeSet) {
+			foreach (var i in nodeSet) {
+				foreach (var j in nodeSet) {
+					var ijEdge = new Edge<T>(i, j);
+					var ikEdge = new Edge<T>(i, k);
+					var kjEdge = new Edge<T>(k, j);
+
+					//dist[i, j] = Math.Min(dist[i, k], dist[i, k] + dist[k, j]);
+					if (dist.TryGetValue(ijEdge, out var ijWeight)) {
+						if (dist.TryGetValue(ikEdge, out var ikWeight) && dist.TryGetValue(kjEdge, out var kjWeight)) {
+							if (ijWeight > ikWeight + kjWeight) {
+								dist[ijEdge] = ikWeight + kjWeight;
+							}
+						}
+					} else {
+						// dist[i, j] is infinite... if we have weights for i,k and k,j then add them, otherwise it stays infinite
+						if (dist.TryGetValue(ikEdge, out var ikWeight) && dist.TryGetValue(kjEdge, out var kjWeight)) {
+							dist.Add(ijEdge, ikWeight + kjWeight);
+						}
+					}
+				}
+			}
+		}
+
+		return dist;
+	}
+
+	public readonly record struct Edge<T>(T From, T To);
+
+	public readonly record struct WeightedEdge<T, W>(Edge<T> Edge, W Weight) where W : INumber<W>
+	{
+		public WeightedEdge(T From, T To, W Weight) : this(new Edge<T>(From, To), Weight) { }
+	}
+}
---
+++ aoc-2022/Day16/Problem.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Specialized;
+
+using static AdventOfCode.FloydWarshall;
+
+namespace aoc_2022.Day16;
+
+public partial class Problem
+{
+	public static (int, int) Main(string fileName)
+	{
+		//fileName = "inputSample.txt";
+
+		var rooms       = ReadFileLines(fileName, Room.Parse).ToDictionary(r => r.Name);
+		var flowRooms   = rooms.Values.Where(r => r.FlowRate > 0 || r.Name == "AA").Select(r => r.Name).ToHashSet();
+		var connections = ComputeDistances(rooms.SelectMany(r => r.Value.Connections.Select(c => new WeightedEdge<string, int>(r.Key, c, 1))))
+			.Where(kvp => kvp.Key.From != kvp.Key.To && flowRooms.Contains(kvp.Key.From) && flowRooms.Contains(kvp.Key.To))
+			.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+		var masks       = new Dictionary<string, int>();
+		var lastMask    = 0;
+
+		foreach (var room in flowRooms) {
+			var nextMask = BitVector32.CreateMask(lastMask);
+			masks.Add(room, nextMask);
+			lastMask = nextMask;
+		}
+
+		var part1 = Search(30, rooms, flowRooms, masks, connections).Values.Max();
+		var bests = Search(26, rooms, flowRooms, masks, connections);
+		var part2 = 0;
+
+		foreach (var best1 in bests) {
+			foreach (var best2 in bests) {
+				if ((best1.Key & best2.Key) == 0) {
+					part2 = Math.Max(part2, best1.Value + best2.Value);
+				}
+			}
+		}
+
+		return (part1, part2);
+	}
+	
+	private static Dictionary<int, int> Search(int minutes, Dictionary<string, Room> rooms, HashSet<string> flowRooms, Dictionary<string, int> masks, Dictionary<Edge<string>, int> connections)
+	{
+		var best   = new Dictionary<int, int>();
+		var seen   = new HashSet<State>();
+		var states = new Queue<State>(new[] {
+			new State(new BitVector32(0), minutes, "AA", 0),
+		});
+
+		while (states.Count > 0) {
+			var state = states.Dequeue();
+
+			if (!best.TryGetValue(state.OpenValves.Data, out var currentBest)) {
+				best.Add(state.OpenValves.Data, state.TotalRelease);
+			} else if (state.TotalRelease > currentBest) {
+				best[state.OpenValves.Data] = state.TotalRelease;
+			}
+
+			if (seen.Contains(state)) {
+				continue;
+			}
+
+			seen.Add(state);
+
+			foreach (var room in flowRooms.Where(r => !state.OpenValves[masks[r]])) {
+				if (room == state.CurrentRoom) {
+					continue;
+				}
+
+				var remaining = state.MinutesRemaining - connections[new Edge<string>(state.CurrentRoom, room)] - 1;
+
+				if (remaining <= 0) {
+					continue;
+				}
+
+				states.Enqueue(new State(new BitVector32(state.OpenValves.Data | masks[room]), remaining, room, state.TotalRelease + remaining * rooms[room].FlowRate));
+			}
+		}
+
+		return best;
+	}
+
+	private readonly record struct Room(string Name, int FlowRate, IEnumerable<string> Connections)
+	{
+		public static Room Parse(string line)
+		{
+			var match = ParseRegex().Match(line);
+
+			if (!match.Success) {
+				throw new InvalidDataException($"Unable to parse input {line}");
+			}
+
+			return new Room(match.Groups["name"].Value, int.Parse(match.Groups["flowrate"].Value), match.Groups["path"].Captures.Select(c => c.Value).ToList());
+		}
+	}
+
+	private readonly record struct State(BitVector32 OpenValves, int MinutesRemaining, string CurrentRoom, int TotalRelease);
+
+	[GeneratedRegex(@"Valve (?<name>\w+) has flow rate=(?<flowrate>\d+); (tunnels lead to valves|tunnel leads to valve) ((?<path>[A-Z]+)(, |))+")]
+	private static partial Regex ParseRegex();
+}
