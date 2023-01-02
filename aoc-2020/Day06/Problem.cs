733524997c90b5a9f7bdb089f6a1e3b782e66272
Mark Parker <mparker@medicity.com>
2023-01-02 08:06:46 -0700

    Add 2020 day 6 solution (from original)

---
+++ aoc-2020/Day06/Problem.cs
@@ -0,0 +1,13 @@
+﻿namespace aoc_2020.Day06;
+
+public class Problem
+{
+	public static (int, int) Main(string fileName)
+	{
+		var input = ReadFileLines(fileName);
+		var part1 = ChunkByEmpty(input).Sum(g => g.SelectMany(l => l.ToCharArray()).Distinct().Count());
+		var part2 = ChunkByEmpty(input).Sum(g => g.Aggregate((IEnumerable<char>)g.First().ToCharArray(), (prev, next) => prev.Intersect(next.ToCharArray())).Count());
+
+		return (part1, part2);
+	}
+}
