f17ba222c61b67bc7142ad2a46725b233757b12f
Mark Parker <mparker@medicity.com>
2023-01-02 07:33:35 -0700

    Add 2020 day 1 solution (from original)

---
+++ aoc-2020/Day01/Problem.cs
@@ -0,0 +1,30 @@
+﻿using Combinatorics.Collections;
+
+namespace aoc_2020;
+
+public class Problem
+{
+	public static (int, int) Main(string fileName)
+	{
+		var input = ReadFileLines(fileName, int.Parse);
+		var pair  = new Combinations<int>(input, 2).First(c => c.Sum() == 2020);
+		var trip  = new Combinations<int>(input, 3).First(c => c.Sum() == 2020);
+
+		return (Multiply(pair), Multiply(trip));
+	}
+
+	public static int Multiply(IEnumerable<int> input)
+	{
+		var v1 = default(int?);
+
+		foreach (var i in input) {
+			if (v1 == null) {
+				v1 = i;
+			} else {
+				v1 *= i;
+			}
+		}
+
+		return v1 ?? 0;
+	}
+}
---
+++ aoc-2020/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace aoc_2020;
+
+public static partial class Program
+{
+	public static async Task Main(string[] args)
+	{
+		await ProblemRunner.Execute(args, Problems());
+	}
+
+	private static partial ProblemMetadata[] Problems();
+}
