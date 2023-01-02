3d40a139a0d3ecca651c94ca71af46e89d84b2f2
Mark Parker <mparker@medicity.com>
2023-01-02 07:51:27 -0700

    Add 2020 day 5 solution (from original)

---
+++ aoc-2020/Day05/Problem.cs
@@ -0,0 +1,38 @@
+﻿namespace aoc_2020.Day05;
+
+public class Problem
+{
+	public static (int, int) Main(string fileName)
+	{
+		var seats = ReadFileLines(fileName, FindSeat);
+		var part1 = seats.Max();
+		var part2 = Enumerable.Range(seats.Min(), seats.Max()).Single(i => seats.Contains(i + 1) && seats.Contains(i - 1) && !seats.Contains(i));
+
+		Console.WriteLine($"part 1 {part1}"); // part 1 is 892
+		Console.WriteLine($"part 2 {part2}"); // part 2 is 625
+
+		return (part1, part2);
+	}
+
+	private static int FindSeat(string pass)
+	{
+		var rs = 0;
+		var re = 127;
+		var cs = 0;
+		var ce = 7;
+
+		foreach (var c in pass) {
+			if (c == 'F') {
+				re -= (re - rs) / 2 + 1;
+			} else if (c == 'B') {
+				rs += (re - rs) / 2 + 1;
+			} else if (c == 'L') {
+				ce -= (ce - cs) / 2 + 1;
+			} else if (c == 'R') {
+				cs += (ce - cs) / 2 + 1;
+			}
+		}
+
+		return (rs * 8) + cs;
+	}
+}
