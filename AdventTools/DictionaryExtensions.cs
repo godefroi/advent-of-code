1e6a3b519c40ddd405f588b01cd8ce45964b5714
Mark Parker <mparker@medicity.com>
2023-01-03 08:47:13 -0700

    Add 2020 day 10 solution (from original, with new part 2)

---
+++ AdventTools/DictionaryExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode;
+
+public static class DictionaryExtensions
+{
+	public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defaultValueProvider) => dictionary.TryGetValue(key, out var value) ? value : defaultValueProvider();
+}
